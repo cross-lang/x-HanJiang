23ff103cbf889103ad5987232fdb2600bc90fcca
John Young <john.young@foxmail.com>
2026-09-13 14:44:18 +0800

    feat:　rename files to file

--- src/api/router.py
+++ src/api/router.py
@@ -9,7 +9,7 @@
 
 from fastapi import APIRouter
 
-from src.api.v1 import audit, auth, files, health, role, security, user
+from src.api.v1 import audit, auth, file, health, role, security, user
 from src.constants import API_PREFIX
 
 api_router = APIRouter(prefix=API_PREFIX)
@@ -30,7 +30,7 @@ api_router.include_router(role.router)
 api_router.include_router(audit.router)
 
 # 注册文件上传路由
-api_router.include_router(files.router)
+api_router.include_router(file.router)
 
 # 注册安全与 MFA 路由
 api_router.include_router(security.router)
